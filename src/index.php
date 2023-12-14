0d2189c8fdd0d3d4392797a26c7094bdfc5200d8
Erwann27 <montagne.erwann.2@gmail.com>
2023-12-14 23:39:12 +0100

    Created navbar

--- src/index.php
+++ src/index.php
@@ -4,14 +4,30 @@
     <meta charset="utf-8" lang="fr">
     <title>Terminologio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <nav class="navbar">
+    <nav class="navbar bg-dark-subtle">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Test</a>
+            <span class="navbar-brand">Bienvenue</span>
+            <?php
+                if(isset($_SESSION)) {
+            ?>
+            <a class="icon-link" href="#">
+                <i class="bi-power" style="font-size: 3ex; color: black;"></i>
+            </a>
+            <?php
+                } else {
+            ?>
+            <a href="#">
+                Se connecter
+            </a>
+            <?php
+                }
+            ?>
         </div>
     </nav>
 </body>
