0bd714c9e8a2f65600905f7580c8e76c085bd4de
Erwann27 <montagne.erwann.2@gmail.com>
2023-12-19 23:25:09 +0100

    Put name in red if user is admin

--- src/Controller/connection_type.php
+++ src/Controller/connection_type.php
@@ -1,9 +1,10 @@
 <?php
+session_start();
 
 
 if (isset($_POST["connection_selection"])) {
     if($_POST["connection_selection"] == 1){
-        $_SESSION["is_admin"] = 1;
+        $_SESSION["is_admin"] = true;
     }
     header("Location:../index.php");
 }
--- src/navbar.php
+++ src/navbar.php
@@ -1,21 +1,28 @@
 <nav class="navbar bg-dark-subtle mb-5">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Bienvenue</a>
-        <?php
-        if (isset($_SESSION["username"])) {
-            echo $_SESSION["username"];
-            ?>
+        <a class="navbar-brand" href="index.php">Bienvenue
+            <span class="<?php
+            if(isset($_SESSION["is_admin"])){
+                echo "text-danger";
+            } ?>">
+                <?php
+                if (isset($_SESSION["username"])) {
+                    echo $_SESSION["username"];
+                    ?>
+                </span>
+            </a>
             <a class="icon-link" href="Controller/disconnect.php">
                 <i class="bi-power" style="font-size: 3ex; color: black;"></i>
             </a>
             <?php
-        } else {
-            ?>
+                } else {
+                    ?>
+            </a>
             <a href="login.php">
                 Se connecter
             </a>
             <?php
-        }
-        ?>
+                }
+                ?>
     </div>
 </nav>
