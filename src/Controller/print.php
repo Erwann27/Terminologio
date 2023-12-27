1a02ce8ed97b4a972f6c887e5937c08b8246a790
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2023-12-27 09:36:11 +0100

    fixed path for images

--- src/Controller/print.php
+++ src/Controller/print.php
@@ -10,7 +10,7 @@ $list = $pic -> getProjectsFromTitleAndCategory($pic -> getTitle(), $category ->
 $result = "<svg xmlns='http://www.w3.org/2000/svg' id='svg' viewBow='0 0 100 100'></svg>";
 if (count($list) != 0) {
     $img = $list[0]["title"];
-    $path = "../res/" . $category->getName() . "/" . $img;
+    $path = "res/" . $category->getName() . "/" . $img;
     $result .= "<img src='$path' alt='$img' id='img' class='img-fluid rounded'>";
 }
 echo $result;
